358f6c0bb00d63264b25ff241a275ae80add55fd
Dvirnyak <artem.dvirnyak@gmail.com>
2021-12-04 00:23:25 +0300

    Fixed c_strings. Now it passes all the tests

--- Artem_Dvirnyak/task2/c_strings.cpp
+++ Artem_Dvirnyak/task2/c_strings.cpp
@@ -1,47 +1,6 @@
 #include <cstdio>
 #include <cstring>
-#include <algorithm>
 
-struct Number {
-    bool sign = true;
-    char* integer = new char[10000];
-    char* fractional = new char[10000];
-    Number(char* s) {
-        if (s[0] == '-') {
-            sign = false;
-            strcpy(s, s+1);
-        }
-        char* pos = strstr(s, ".");
-        if (pos != nullptr) {
-            strncpy(integer, s, strlen(s)-strlen(pos));
-            //удаляем нули с конца
-            strcpy(s, pos+1);
-            int end = -1;
-            for (int i = strlen(s)-1; i >= 0; --i) {
-                if (s[i] != '0') {
-                    end = i;
-                    break;
-                }
-            }
-            strncpy(fractional, s, end+1);
-        } else {
-            integer = s;
-        }
-    }
-};
-bool is_first_bigger(char* a, char* b, bool is_integer = true) {
-    if (is_integer && strlen(a) != strlen(b)) {
-        return strlen(a) > strlen(b);
-    } else if (strlen(a) != strlen(b)) {
-        for (size_t i = 0; i < std::min(strlen(a),strlen(b)); ++i) {
-            if (a[i] != b[i]) {
-                return a[i] > b[i];
-            }
-        }
-        return strlen(a) > strlen(b);
-    }
-    return (strcmp(a,b) + 1) / 2;//convert -1 and +1 to bool
-}
 char output(bool type, bool sign = true) {
     if (type == sign) {
         return '>';
@@ -49,33 +8,41 @@ char output(bool type, bool sign = true) {
     return '<';
 }
 char cmp(char* first, char* second) {
-    Number first_number(first);
-    Number second_number(second);
-    //равенство
-    if (strcmp(first_number.integer, second_number.integer) == 0
-        && strlen(first_number.integer) == strlen(second_number.integer)
-        && strcmp(first_number.fractional, second_number.fractional) == 0
-        && strlen(first_number.fractional) == strlen(second_number.fractional)
-        && first_number.sign == second_number.sign) {
-        return '=';
+    //только одно число положительно
+    if (static_cast<int>(first[0] == '-') + static_cast<int>(second[0] == '-') == 1) {
+        return output(first[0] != '-');
     }
-    // только одно из чисел положительно
-    if ((int)first_number.sign + (int)second_number.sign == 1) {
-        return output(first_number.sign);
+    size_t pos1 = (strchr(first, '.') == nullptr) ? strlen(first) : strchr(first, '.') - &first[0];
+    size_t pos2 = (strchr(second, '.') == nullptr) ? strlen(second) : strchr(second, '.') - &second[0];
+    //целая часть длиннее
+    if (pos1 != pos2) {
+        return output((pos1 > pos2), (first[0] != '-'));
     }
-    //целые части совпали
-    if (strcmp(first_number.integer, second_number.integer) == 0) {
-        return output(is_first_bigger(first_number.fractional,
-                                      second_number.fractional,false),
-                      first_number.sign);
+    //целая часть больше
+    if (strncmp(first, second, pos1) != 0) {
+        return output(strncmp(first, second, pos1) > 0, (first[0] != '-'));
     }
-    return output(is_first_bigger(first_number.integer,
-                                  second_number.integer),
-                  first_number.sign);
+    //сравниваем дробные части
+    size_t i = pos1+1;
+    for ( ; i < strlen(first) && i < strlen(second); ++i) {
+        if (first[i] != second[i]) {
+            return output(first[i] > second[i], first[0] != '-');
+        }
+    }
+    // одна дробь закончилась, другая - нет
+    for ( ; i < strlen(first) || i < strlen(second); ++i) {
+        if (strlen(first) > i && first[i] != '0') {
+            return output(true, first[0] != '-');
+        }
+        if (strlen(first) > i && first[i] != '0') {
+            return output(false, first[0] != '-');
+        }
+    }
+    return '=';
 }
 int main() {
-    char first[10000];
-    char second[10000];
+    char first[10001];
+    char second[10001];
     scanf("%s%s", first, second);
     printf("%c", cmp(first, second));
 }
