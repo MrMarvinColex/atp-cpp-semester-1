faf2493d60d0849ae3b1bfe515fb0dcf5f0101a6
Stase <staseann@gmail.com>
2021-12-16 08:43:14 +0300

    edited string.h

--- Anastasiia_Ivanova/lecture_hw_string/string.h
+++ Anastasiia_Ivanova/lecture_hw_string/string.h
@@ -6,51 +6,20 @@ size_t max(size_t value1, size_t value2) {
 }
 
 class String {
- private:
-  size_t stringLength;
-  const size_t MIN_CAPACITY = 15;
-  size_t capacity;
-  char* charArray;
-
-  [[nodiscard]] size_t commonFind(const String& anotherString, bool reverse) const {
-    if (anotherString.length() > stringLength) {
-      return stringLength;
-    }
-    for (size_t i = 0; i < stringLength - anotherString.length() + 1; ++i) {
-      bool isEqual = true;
-      for (size_t j = 0; j < anotherString.length(); ++j) {
-        if ((!reverse && anotherString[j] != charArray[i + j]) ||
-            (reverse && anotherString[anotherString.length() - 1 - j] != charArray[stringLength - 1 - i - j])) {
-          isEqual = false;
-          break;
-        }
-      }
-      if (isEqual) {
-        return reverse ? stringLength - i - anotherString.length() : i;
-      }
-    }
-    return stringLength;
-  }
-  void resize(size_t newCapacity) {
-    capacity = newCapacity;
-    char* copy = charArray;
-    charArray = new char[newCapacity];
-    memcpy(charArray, copy, stringLength);
-    delete[] copy;
-  }
-
  public:
+  friend std::istream& operator>>(std::istream& in, String& string);
+  friend std::ostream& operator<<(std::ostream& out, const String& string);
   String(const char* initArray) : stringLength(strlen(initArray)),
-                                  capacity(max(stringLength * 2, MIN_CAPACITY)), charArray(new char[capacity]) {
+                                  capacity(max(stringLength, MIN_CAPACITY)), charArray(new char[capacity]) {
     memcpy(charArray, initArray, stringLength);
   }
   explicit String(size_t n, char c) : stringLength(n),
-                                      capacity(max(stringLength * 2, MIN_CAPACITY)), charArray(new char[capacity]) {
+                                      capacity(max(stringLength, MIN_CAPACITY)), charArray(new char[capacity]) {
     memset(charArray, c, stringLength);
   }
   explicit String() : stringLength(0), capacity(MIN_CAPACITY), charArray(new char[capacity]) {}
   String(const String& anotherString) : stringLength(anotherString.length()),
-                                        capacity(max(anotherString.capacity, MIN_CAPACITY)),
+                                        capacity(max(stringLength, MIN_CAPACITY)),
                                         charArray(new char[capacity]) {
     memcpy(charArray, anotherString.charArray, stringLength);
   }
@@ -70,38 +39,31 @@ class String {
     memcpy(charArray, anotherString.charArray, stringLength);
     return *this;
   }
-
-  const char& operator[](const size_t index) const {
+  const char& operator[](size_t index) const {
     return charArray[index];
   }
   char& operator[](size_t index) {
     return charArray[index];
   }
-
   [[nodiscard]] size_t length() const {
     return stringLength;
   }
   [[nodiscard]] bool empty() const {
     return stringLength == 0;
   }
-
   char& front() {
     return charArray[0];
   }
   [[nodiscard]] const char& front() const {
-
     return charArray[0];
   }
   char& back() {
     return charArray[stringLength - 1];
   }
   [[nodiscard]] const char& back() const {
-
     return charArray[stringLength - 1];
   }
-
   [[nodiscard]] size_t find(const String& anotherString) const {
-
     return commonFind(anotherString, false);
   }
   [[nodiscard]] size_t rfind(const String& anotherString) const {
@@ -114,7 +76,6 @@ class String {
     }
     return subString;
   }
-
   void push_back(char element) {
     if (stringLength + 1 >= capacity) {
       resize(max(capacity * 2 + 1, stringLength + 2));
@@ -142,12 +103,39 @@ class String {
     for (size_t i = 0; i < end; ++i) {
       push_back(anotherString[i]);
     }
-
     return *this;
   }
-
-  friend std::istream& operator>>(std::istream& in, String& string);
-  friend std::ostream& operator<<(std::ostream& out, const String& string);
+ private:
+  size_t stringLength;
+  const size_t MIN_CAPACITY = 15;
+  size_t capacity;
+  char* charArray;
+  [[nodiscard]] size_t commonFind(const String& anotherString, bool reverse) const {
+    if (anotherString.length() > stringLength) {
+      return stringLength;
+    }
+    for (size_t i = 0; i < stringLength - anotherString.length() + 1; ++i) {
+      bool isEqual = true;
+      for (size_t j = 0; j < anotherString.length(); ++j) {
+        if ((!reverse && anotherString[j] != charArray[i + j]) ||
+            (reverse && anotherString[anotherString.length() - 1 - j] != charArray[stringLength - 1 - i - j])) {
+          isEqual = false;
+          break;
+        }
+      }
+      if (isEqual) {
+        return reverse ? stringLength - i - anotherString.length() : i;
+      }
+    }
+    return stringLength;
+  }
+  void resize(size_t newCapacity) {
+    capacity = newCapacity;
+    char* copy = charArray;
+    charArray = new char[newCapacity];
+    memcpy(charArray, copy, stringLength);
+    delete[] copy;
+  }
 };
 
 String operator+(const String& string, const char element) {
@@ -165,7 +153,6 @@ String operator+(const String& string1, const String& string2) {
   copyString += string2;
   return copyString;
 }
-
 bool operator==(const String& s1, const String& s2) {
   if (s1.length() != s2.length()) {
     return false;
@@ -177,7 +164,6 @@ bool operator==(const String& s1, const String& s2) {
   }
   return true;
 }
-
 std::ostream& operator<<(std::ostream& out, const String& string) {
   for (size_t i = 0; i < string.stringLength; ++i) {
     out << string.charArray[i];
